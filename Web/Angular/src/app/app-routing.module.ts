b68143a1d3963b6cd3dfae73525dbfff95069702
Robson Jesus de Souza <robsonjesus908@gmail.com>
2021-08-29 11:22:18 -0300

    Atualização rotas

--- Web/Angular/src/app/app-routing.module.ts
+++ Web/Angular/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'shop', component: ShoppingCartComponent },
-  { path: '**', component: PageNotFoundComponent }
+  //{ path: '**', component: PageNotFoundComponent }
 ]
 
 @NgModule({
